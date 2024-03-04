660435a0882a5f4c0b54716f29613838cce2486b
Vishal Sehgal <40015610+iamvishalsehgal@users.noreply.github.com>
2024-03-04 11:38:54 +0100

    Added Middleware, proxies

--- crawler/crawler/spiders/crawling_spider.py
+++ crawler/crawler/spiders/crawling_spider.py
@@ -1,10 +1,15 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
+proxies = { 
+     'http': 'localhost:8118',
+     'https': 'localhost:8118'
+}
+
 class CrawlingSpider(CrawlSpider):
-    name = "Crawler"
-    allowed_domains = ["uvt.nl"] # domain link
-    start_urls = ["https://libsearch.uvt.nl/"] # website link 
+    name = "AVCrawler"
+    allowed_domains = ["muwgjdckwwmhyi7lj73dspumrxmzuzjvujmtmyrhhbjrgswcakobtfad.onion"] # domain link
+    start_urls = ["https://muwgjdckwwmhyi7lj73dspumrxmzuzjvujmtmyrhhbjrgswcakobtfad.onion/"] # website link 
 
     rules = (
         # all here is the https://libsearch.uvt.nl/all/ loction where books are kinda
